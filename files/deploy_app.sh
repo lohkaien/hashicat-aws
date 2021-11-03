bc44b342fe81b487c3703009d251f2b8604ca660
Kai-En Loh <lohkaien@gmail.com>
2021-11-03 10:59:56 +0800

    Update some custom marketing text

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. This is a Lab Exercise to update some custom text here!
   <!-- END -->
 
   </div>
