62fc4214d74d85d59d4e49099231355caf73336a
Kai-En Loh <lohkaien@gmail.com>
2021-11-03 12:12:52 +0800

    Add s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "app.terraform.io/lohkaien/s3-bucket/aws"
+  source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.2.0"
   bucket_prefix = "terraform-workshop-s3bucket"
   bucket = "my-s3-bucket"
